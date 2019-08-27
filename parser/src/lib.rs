2020edb5244be772325d25ac0f26a358821cc991
Michael Alexander <beefsack@gmail.com>
2019-08-27 13:05:03 +1000

    Initial parser and types

---
+++ parser/src/lib.rs
@@ -0,0 +1,109 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub enum Hero {
+    Ana,
+    Ashe,
+    Baptiste,
+    Bastion,
+    Brigitte,
+    DVa,
+    Doomfist,
+    Genji,
+    Hanzo,
+    Junkrat,
+    Lucio,
+    McCree,
+    Mei,
+    Mercy,
+    Moira,
+    Orisa,
+    Pharah,
+    Reaper,
+    Reinhardt,
+    Roadhog,
+    Sigma,
+    Soldier76,
+    Sombra,
+    Symmetra,
+    Torbjorn,
+    Tracer,
+    Widowmaker,
+    Winston,
+    WreckingBall,
+    Zarya,
+    Zenyatta,
+}
+
+#[derive(Deserialize)]
+pub enum Map {
+    HanamuraAttack,
+    HanamuraDefense,
+    HorizonLunarColonyAttack,
+    HorizonLunarColonyDefense,
+    ParisAttack,
+    ParisDefense,
+    TempleOfAnubisAttack,
+    TempleOfAnubisDefense,
+    VolskayaIndustriesAttack,
+    VolskayaIndustriesDefense,
+    DoradoAttack,
+    DoradoDefense,
+    JunkertownAttack,
+    JunkertownDefense,
+    RialtoAttack,
+    RialtoDefense,
+    Route66Attack,
+    Route66Defense,
+    WatchpointGibraltarAttack,
+    WatchpointGibraltarDefense,
+    BlizzardWorldAttack,
+    BlizzardWorldDefense,
+    EichenwaldeAttack,
+    EichenwaldeDefense,
+    HollywoodAttack,
+    HollywoodDefense,
+    KingsRowAttack,
+    KingsRowDefense,
+    BusanSanctuary,
+    BusanDowntown,
+    BusanMEKABase,
+    IliosLighthouse,
+    IliosWell,
+    IliosRuins,
+    LijiangTowerNightMarket,
+    LijiangTowerGarden,
+    LigiangTowerControlCenter,
+    NepalVillage,
+    NepalShrine,
+    NepalSanctum,
+    OasisCityCenter,
+    OasisGardens,
+    OasisUniversity,
+}
+
+#[derive(Deserialize)]
+pub struct Rule {
+    pub comment: String,
+    pub allies: Vec<Hero>,
+    pub enemies: Option<Vec<Hero>>,
+    pub maps: Option<Vec<Map>>,
+    pub score: isize,
+}
+
+pub fn from_slice<'de>(bytes: &'de [u8]) -> Result<HashMap<String, Rule>, toml::de::Error> {
+    toml::from_slice(bytes)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn data_works() {
+        let raw = include_bytes!("../../data/rules.toml");
+        let _rules = from_slice(raw).expect("expected to read TOML file");
+    }
+}
